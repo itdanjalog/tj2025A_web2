33b487836d2c6cc3203a64926ff6f66f21552d27
itdanjalog <kgs2072@naver.com>
2025-09-16 12:35:21 +0900

    day80 리액트 실습1

---
+++ src/main/reactapp/src/example/day01/Task1.jsx
@@ -0,0 +1,21 @@
+// REACT 실습1 : Task1과 Profile 컴포넌트를 완성하시오.
+
+// Fecth 이용하여 서버로 부터 받은 데이터/자료 
+const data = [
+  {
+    name: 'Hedy Lamarr',
+    imageUrl:'https://i.pravatar.cc/150?img=47'
+  },
+  {
+    name: 'Grace Hopper',
+    imageUrl: 'https://i.pravatar.cc/150?img=48'
+  }
+];
+
+export default function Task1( props ){
+  return (<> </>)
+}
+
+function Profile( props ) {
+  return (<> </> );
+}
