0a10240f2f396f51c0a2cc9f51ba57b9fc31e6f3
itdanjalog <kgs2072@naver.com>
2025-09-18 15:37:28 +0900

    day81 리액트

--- src/main/reactapp/src/example/day03/Task5.jsx
+++ src/main/reactapp/src/example/day03/Task5.jsx
@@ -1,5 +1,5 @@
 
-// TASK5 : 기존 TASK4.jsx 이어서 useEffect/axios를 활용해서 spring+mybatis 서버 와 통신하여 TASK5 완성(등록/전체조회/삭제)하시오.
+// TASK5 : 기존 TASK4.jsx 이어 useEffect/axios를 활용해서 spring+mybatis 서버 와 통신하여 TASK5 완성(등록/전체조회/삭제)하시오.
 
 export default function Task5( props ){
     const [ name , setName ] = useState('')
