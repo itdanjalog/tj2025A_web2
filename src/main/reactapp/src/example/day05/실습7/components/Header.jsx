0a34737cb6003e17c744e030daf242bc96e481ca
itdanjalog <kgs2072@naver.com>
2025-09-23 15:29:48 +0900

    day84 리액트 실습7:리덕스+라우터

--- src/main/reactapp/src/example/day05/실습7/components/Header.jsx
+++ src/main/reactapp/src/example/day05/실습7/components/Header.jsx
@@ -1,14 +1,29 @@
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../store/userSlice.jsx'
 export default function Header( props ){
+    // [1] store 저장된 상태를 가져오기 , useSelector( (state)=>state.상태명 )
+    const { isAuthenticated } = useSelector( (state)=> state.user );        console.log( isAuthenticated );
+    // [2] dispatch 이용한 상태 변경 하기 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const onLogout = async()=>{
+        alert('[로그아웃 성공]');
+        dispatch( logout() ); // 상태가 변경되면 **리렌더링** 된다.
+        navigate('/login')
+    }
     return(<>
-        <div>
-            <h3> 헤더 </h3>
-            <ul>
-                <li> <Link to ="/"> 홈 </Link></li>
-                <li> <Link to ="/login"> 로그인 </Link></li>
-                <li> <Link to ="/profile"> 프로필 </Link></li>
-            </ul>
-        </div>
+        <h3> 헤더 </h3>
+        <ul>
+            <li> <Link to ="/"> 홈 </Link></li>
+            { isAuthenticated==true ? 
+                <> 
+                    <li> <Link to ="/profile"> 프로필 </Link></li>
+                    <li> <button onClick={ onLogout }> 로그아웃 </button></li>
+                </> 
+                : 
+                <>  <li> <Link to ="/login"> 로그인 </Link></li> </> 
+            }
+        </ul>
     </>)
 }
