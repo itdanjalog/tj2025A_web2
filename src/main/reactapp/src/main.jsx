464623c7bd15af4fa143aae802aba354d02dd92f
itdanjalog <kgs2072@naver.com>
2025-09-23 10:22:19 +0900

    day84 리액트 리덕스

--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -62,5 +62,7 @@ import Component11 from './example/day04/Component11.jsx'
 // create.render( <Component12 /> );
 
 import Task6 from './example/day04/Task6.jsx'
-create.render( <Task6 /> );
+create.render( 
+    <Task6 /> 
+);
 
