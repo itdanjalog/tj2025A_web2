461c487e7699848269a5b2d5e99f4151bc83c242
itdanjalog <kgs2072@naver.com>
2025-09-25 12:26:03 +0900

    day86 트랜잭션

--- src/main/java/example/day09/TransMapper.java
+++ src/main/java/example/day09/TransMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface TransMapper {
     // (1) insert
-    @Insert( "insert into trans( name ) values( # {name} ) ")
+    @Insert( "insert into trans( name ) values( #{name} ) ")
     public boolean trans1(String name );
 }
 
