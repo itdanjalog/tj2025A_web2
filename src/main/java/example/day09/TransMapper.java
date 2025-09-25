b669354928cd545070743dd59f154fb291463039
itdanjalog <kgs2072@naver.com>
2025-09-25 12:40:54 +0900

    day86 트랜잭션

--- src/main/java/example/day09/TransMapper.java
+++ src/main/java/example/day09/TransMapper.java
@@ -5,9 +5,13 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TransMapper {
-    // (1) insert
-    @Insert( "insert into trans( name ) values( #{name} ) ")
+    // (1) insert1 : 정상
+    @Insert( "insert into trans( name ) values( #{name} )")
     public boolean trans1(String name );
+    // (1) insert2 : 비정상
+    @Insert( "insert into trans( name ) 오타( #{name} ")
+    public boolean trans2( String name );
+
 }
 
 
--- src/main/java/example/day09/TransService.java
+++ src/main/java/example/day09/TransService.java
@@ -2,6 +2,7 @@ package example.day09;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -9,11 +10,12 @@ public class TransService {
     private final TransMapper transMapper;
     // 1. '유재석' 과 '강호동' insert 하는게 목적(commit)
     // '만약에' 한명이라도 insert가 실패하면 취소(rollback)
+    @Transactional // 지정한 함수내 모든 SQL은 트랜잭션 적용
     public boolean trans1(){
-        // 1-1 유재석 insert 하고
+        // 1-1 유재석 insert1 하고
         transMapper.trans1( "유재석" );
-        // 1-2 강호동 insert 한다.
-        transMapper.trans1( "강호동" );
+        // 1-2 강호동 insert2 한다.
+        transMapper.trans2( "강호동" );
         return true;
     }
 }
