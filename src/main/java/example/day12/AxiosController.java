035b71009663f64d2d872fa474cbf20f7c64cfe1
itdanjalog <kgs2072@naver.com>
2025-09-30 12:03:20 +0900

    day89 CORS 설정

--- src/main/java/example/day12/AxiosController.java
+++ src/main/java/example/day12/AxiosController.java
@@ -1,9 +1,9 @@
 package example.day12;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/axios")
@@ -13,6 +13,23 @@ public class AxiosController {
     public int axios1(){
         System.out.println("AxiosController.axios1");
         return 10;
+    }
+    // [2]
+    @PostMapping("/login")
+    public boolean axios2(
+            @RequestBody Map<String, String> map ,
+            HttpSession session ){
+        String id = map.get("id");
+        // 2-1 로그인 세션의 속성 등록
+        session.setAttribute("loginId" , id );
+        return true;
+    }
+    // [3]
+    @GetMapping("/info")
+    public boolean axios3( HttpSession session ){
+        Object object = session.getAttribute("loginId");
+        if( object == null ) return false; // 비로그인중
+        return true; // 로그인중
     }
 }
 
--- src/main/reactapp/src/example/day08/Component15.jsx
+++ src/main/reactapp/src/example/day08/Component15.jsx
@@ -5,15 +5,45 @@ export default function Component15(props){
     // [1]
     const axios1 = async()=>{
         try{
-            const response
-                = await axios.get("http://localhost:8080/axios");
+            const response = await axios.get("http://localhost:8080/axios");
             const data = response.data;
             console.log( "[1] : " , data );
         }catch(e){ console.log( e ) }
     }
-
+    // [2] 로그인
+    const axios2 = async()=>{
+        try{
+            const obj = { id : "qwe" , password : "1234"}
+            const response = await axios.post( "http://localhost:8080/axios/login" , obj )
+            const data = response.data;
+            console.log( "[2] : " , data );
+        }catch(e){console.log(e)}
+    }
+    // [3] 내정보
+    const axios3 = async()=>{
+        try{
+            const response = await axios.get( "http://localhost:8080/axios/info" );
+            const data = response.data;
+            console.log( "[3] : " , data );
+        }catch(e){console.log(e)}
+    }
     return(<>
         <h5> AXIOS 테스트 </h5>
         <button onClick={ axios1 }> axios1 </button>
+        <button onClick={ axios2 }> axios2 </button>
+        <button onClick={ axios3 }> axios3 </button>
     </>)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
