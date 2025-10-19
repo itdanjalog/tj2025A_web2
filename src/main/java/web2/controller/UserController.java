6d7d31f3e6667e2f77c66623e629c16ace5b1340
MSI <kgs2072@naver.com>
2025-10-20 00:07:31 +0900

    -

--- src/main/java/web2/controller/UserController.java
+++ src/main/java/web2/controller/UserController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
