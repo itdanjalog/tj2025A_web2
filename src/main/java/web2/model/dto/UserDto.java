f14bfd04e809ebbce9917db2a7b38013db7e7557
itdanjalog <kgs2072@naver.com>
2025-10-20 13:23:40 +0900

    day96

---
+++ src/main/java/web2/model/dto/UserDto.java
@@ -0,0 +1,14 @@
+package web2.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private int uno;
+    private String uid;
+    private String upwd ;
+    private String uname ;
+    private String uphone ;
+    private String urole ;
+    private String udate ;
+}
