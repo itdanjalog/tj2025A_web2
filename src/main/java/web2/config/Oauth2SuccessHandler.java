d5a98dca8c975fbd7946cbe9f4f8d78ae0d29211
itdanjalog <kgs2072@naver.com>
2025-10-23 13:43:24 +0900

    day99 oauth2

--- src/main/java/web2/config/Oauth2SuccessHandler.java
+++ src/main/java/web2/config/Oauth2SuccessHandler.java
@@ -1,6 +1,7 @@
 package web2.config;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import web2.service.JwtService;
+import web2.service.UserService;
 
 import java.io.IOException;
 
@@ -16,12 +19,14 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class Oauth2SuccessHandler implements AuthenticationSuccessHandler {
 
+    private final JwtService jwtService; // JWT 서비스
+    private final UserService userService;
+
     // [1] AuthenticationSuccessHandler 구현체를 만든다.
     // [2] onAuthenticationSuccess 메소드 구현한다.
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         // 타사 *로그인 성공 이후* 로직 커스텀( 실패 커스텀 없다 )
-
         System.out.println( authentication ); // authentication 인증 ( 토큰 , 개인정보 등 )
 
         // [3] 로그인 성공한 회원의 타사 발급한 토큰 확인
@@ -34,16 +39,32 @@ public class Oauth2SuccessHandler implements AuthenticationSuccessHandler {
         OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal(); // Principal() 주체(로그인성공한정보)
         System.out.println("oauth2User = " + oauth2User);
 
-        // 1. 어느 타사의 로그인 성공 인지 확인
-        // 2. 로그인 성공한 동의항목(정보) 가져오기 , 개인정보(아이디,회원명,주소,전화번호)
-        // 3. 자사(우리)서버와 타사서버 통합 로그인( web2 : 토큰/쿠키 발급 )
-        // 4. 자사(우리)서버와 타사서버 통합 DB( 최초 로그인이면 DB저장 , 아니면 DB처리 없음 )
+        // [4] google / kakao 인지 식별 , 서로 다른 회사별 동의항목(다르다)
+        String provider = authToken.getAuthorizedClientRegistrationId(); // 등록된 공급자 ID : google , kakao 등등
+        System.out.println("provider = " + provider);
 
-    }
-}
+        String uid = null ; String name = null; // 동의항목 정보
+        if( provider.equals("google") ){
+            uid = oauth2User.getAttribute("email"); // oauth2User.getAttribute("동의항목명")
+            name = oauth2User.getAttribute("name");
+        }
 
+        // [7] oauth2 정보를 데이터베이스 저장
+        userService.oauth2UserSignup( uid , name );
 
+        // [5] 자사(우리)의 로그인 방식 통합 , 권한은 USER 정의
+        Cookie cookie = new Cookie( "loginUser" , jwtService.createToken( uid , "USER") );
+        cookie.setHttpOnly( true ); cookie.setSecure(false);
+        cookie.setPath("/"); cookie.setMaxAge( 60 * 60 );
+        response.addCookie( cookie );
 
+        // [6] 로그인 성공시 어디로 이동할지 ( 프론트엔드 루트 )
+        // response.sendRedirect("http://localhost:5173/"); // 리액트 서버
+        response.sendRedirect("/"); // 자바서버 메인 경로 localhost:8080
+
+    } // m end
+
+} // c end
 
 
 
--- src/main/java/web2/config/SecurityConfig.java
+++ src/main/java/web2/config/SecurityConfig.java
@@ -24,28 +24,28 @@ public class SecurityConfig {
     @Bean // 빈 등록
     public SecurityFilterChain filterChain( HttpSecurity http ) throws Exception{
 
-                // [1] HTTP 요청에 따른 권한 커스텀
-                // .authorizeHttpRequests( auth -> auth.requestMatchers("경로").권한 );
-                    // .permitAll() : 모든 권한 (공개용) 허용
-                    // .hasRole("권한명") , .hasAnyRole("권한명", "권한명");
+        // [1] HTTP 요청에 따른 권한 커스텀
+        // .authorizeHttpRequests( auth -> auth.requestMatchers("경로").권한 );
+        // .permitAll() : 모든 권한 (공개용) 허용
+        // .hasRole("권한명") , .hasAnyRole("권한명", "권한명");
         http.authorizeHttpRequests( auth -> auth
                 .requestMatchers("/api/user/info").hasAnyRole( "USER", "ADMIN") // 2개이상 가능, 권한명은 대문자 권장
                 .requestMatchers("/api/admin/**").hasRole("ADMIN") // admin 관련 controller는 "ADMIN"권한일때 만가능
                 .requestMatchers("/**").permitAll() ); // 모든 권한 허용은 항상 최하단에 정의한다.
 
-                // [2] HTTP 요청에 csrf( 요청간의 해킹 공격 ) POST,PUT 자동 차단 커스텀
-                // http.csrf( csrf -> csrf.ignoringRequestMatchers("csrf제외할경로") ); // 운영단계 권장
+        // [2] HTTP 요청에 csrf( 요청간의 해킹 공격 ) POST,PUT 자동 차단 커스텀
+        // http.csrf( csrf -> csrf.ignoringRequestMatchers("csrf제외할경로") ); // 운영단계 권장
         http.csrf( csrf -> csrf.disable() ); // 계발단계 권장 , csrf 사용안함.
 
-                // [3] 시큐리티 내부에서 사용되는 (세션)토큰 , UsernamePasswordAuthenticationToken
-                // web2 실습에서는 쿠키 기반의 토큰 구현중 = 시큐리티가 제공하는 세션 사용안함
-                // [3-1] 시큐리티 세션 끄기
+        // [3] 시큐리티 내부에서 사용되는 (세션)토큰 , UsernamePasswordAuthenticationToken
+        // web2 실습에서는 쿠키 기반의 토큰 구현중 = 시큐리티가 제공하는 세션 사용안함
+        // [3-1] 시큐리티 세션 끄기
         http.sessionManagement( s -> s.sessionCreationPolicy( SessionCreationPolicy.STATELESS ) );
-                // [3-2] UsernamePasswordAuthenticationToken 을 개발자가 만든 토큰 대체
+        // [3-2] UsernamePasswordAuthenticationToken 을 개발자가 만든 토큰 대체
         // http.addFilterBefore( 내가만든토큰객체필터 , UsernamePasswordAuthenticationFilter.class );
         http.addFilterBefore( jwtAuthFilter , UsernamePasswordAuthenticationFilter.class );
 
-                // [4] Oauth2 로그인 필터 사용 설정
+        // [4] Oauth2 로그인 필터 사용 설정
         // http.oauth2Login( 매개변수 -> 매개변수.successHandler( 로그인성공시특정클래스이동 ) );
         http.oauth2Login( o -> o
                 .loginPage("/oauth2/authorization/google") // 현재 서버의 로그인페이지가 아닌 타사 로그인페이지 사용
