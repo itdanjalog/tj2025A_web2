994fe1115c6ddbe50df6b89351335e069908559f
itdanjalog <kgs2072@naver.com>
2025-11-04 11:41:15 +0900

    day107 JPA

--- src/main/java/example2/day02/GoodsController.java
+++ src/main/java/example2/day02/GoodsController.java
@@ -1,6 +1,9 @@
 package example2.day02;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/goods")
 @RequiredArgsConstructor
 public class GoodsController {
+    private final GoodsService goodsService;
+    // 1. 저장
+    @PostMapping
+    public ResponseEntity<?> goodsSave( @RequestBody GoodsDto goodsDto  ){
+        return ResponseEntity.ok( goodsService.goodsSave( goodsDto ) );
+    }
+
+    // 2. 전체조회
+
+    // 3. 개별조회
+
+    // 4. 개별삭제
+
+    // 5. 개별수정
 
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/example2/day02/GoodsService.java
+++ src/main/java/example2/day02/GoodsService.java
@@ -8,5 +8,31 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class GoodsService {
+    private final GoodsRepository goodsRepository;
+    // 1. 저장
+    public GoodsDto goodsSave( GoodsDto goodsDto ){
+        // 1. 저장할 dto를 매개변수로 받는다.
+        GoodsEntity entity = goodsDto.toEntity();// 2. 저장할 dto를 entity 변환한다.
+        GoodsEntity savedEntity = goodsRepository.save( entity );// 3. .save() 이용한 엔티티 영속화(저장) 하기
+        // 4. 만약에 pk가 생성 되었으면 생성된 엔티티를 dto로 변환하여 반환
+        if( savedEntity.getGno() >= 0 ){ return savedEntity.toDto(); }
+        return goodsDto;
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
